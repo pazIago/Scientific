c965a47255ee9656cd76fa4a3cac1ea4f1a6ad99
giovani.franz <giovani.franz@agi.com.br>
2023-04-12 18:14:10 -0300

    fix/instagram

--- src/components/Instagram/index.tsx
+++ src/components/Instagram/index.tsx
@@ -2,22 +2,28 @@ import { useState, useEffect, useCallback } from "react";
 import Frame from "./Frame";
 import axios from "axios";
 
-interface InstagramPost {
+type Post = {
   id: string;
   permalink: string;
   media_url: string;
   thumbnail_url?: string;
+};
+
+interface InstagramPost {
+  data: Post[];
 }
 
 export default function InstagramFeed() {
-  const [data, setData] = useState<InstagramPost[]>([]);
+  const [data, setData] = useState<Post[]>([]);
+
+  console.log(data);
 
   const getData = useCallback(async () => {
     try {
-      const { data } = await axios.get<InstagramPost[]>(
+      const { data: response } = await axios.get<InstagramPost>(
         "https://graph.instagram.com/me/media?fields=id,permalink,media_url,thumbnail_url,caption&access_token=IGQVJVdWRxNFp3Y0JSbV83RVY1a3RwR3pjTEx6d0NIWGNQVTBleVpJclBXdk9GTW1oRzFiZAi1mTTFnS0JVQ0w2M1hvZA1NMcm1mLTJwLXZA0Y2RnYWh5UkN5TGJ6NElxZAlRSbXhZAbzdBckU3Y0tiSEtIagZDZD"
       );
-      setData(data);
+      setData(response.data);
     } catch (err) {
       console.error(err, "Failed to fetch data");
     }
@@ -25,7 +31,7 @@ export default function InstagramFeed() {
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [getData]);
 
   return (
     <div className="flex flex-wrap justify-center w-full">
