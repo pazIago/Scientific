ec37fde623682a91ed6177e1a806e6296c787e11
giovani.franz <giovani.franz@agi.com.br>
2023-04-13 10:44:40 -0300

    fix/cdn in next config

--- next.config.js
+++ next.config.js
@@ -5,18 +5,20 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development",
   },
-  remotePatterns: [
-    {
-      protocol: "https",
-      pathname: "scontent-iad3-1.cdninstagram.com",
-      protocol: "https",
-      hostname: "scontent-iad3-2.cdninstagram.com",
-      protocol: "https",
-      pathname: "scontent-iad3-3.cdninstagram.com",
-      port: "https",
-      pathname: "instagram.com",
-    },
-  ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        pathname: "scontent-iad3-1.cdninstagram.com",
+        protocol: "https",
+        hostname: "scontent-iad3-2.cdninstagram.com",
+        protocol: "https",
+        pathname: "scontent-iad3-3.cdninstagram.com",
+        port: "https",
+        pathname: "instagram.com",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
